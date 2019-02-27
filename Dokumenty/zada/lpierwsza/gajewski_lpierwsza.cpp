02399dfdca555147a60b6f8ea2bba2753ac4a185
Hubert07 <gajewski.hubert95@gmail.com>
2019-02-27 19:53:31 +0100

    porządek

---
+++ Dokumenty/zada/lpierwsza/gajewski_lpierwsza.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+void lpierwsza(int n) {
+    int i = 2;
+    for (i; i <= n; i++) {
+
+        }
+}
+
+int main(int argc, char **argv) {
+    int n;
+    cout << "podaj liczbę: ";
+    cin >> n;
+    lpierwsza(n);
+    return 0;
+}
+
